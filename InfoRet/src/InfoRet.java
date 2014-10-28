c256863f8568ff2daac3ce7fda68a73138452634
Ben Coble <ben.j.coble@gmail.com>
2014-10-28 11:27:01 -0400

    Added run configuration and comments.

--- InfoRet/src/InfoRet.java
+++ InfoRet/src/InfoRet.java
@@ -3,11 +3,30 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
+
+/*
+ * Possible run configurations-
+ * <query> <score algorithm> <path to corpus directory>
+ * abraham 1 C:\Users\coblebj.000\Documents\Courses\AI\blackbird\Presidents
+ * 
+ */
+
+
+
+/**
+ * 
+ * @author coblebj
+ *
+ */
 public class InfoRet {
 
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ArrayList<JSONObject> corpus = new ArrayList<JSONObject>();
 
 		if (args.length == 3) {
@@ -49,7 +68,7 @@ public class InfoRet {
 
 		ArrayList<JSONObject> results;
 		String query = args[0];
-		System.out.println(query);
+
 		switch (Integer.valueOf(args[1])) {
 		case (1):
 			results = BM25(query, corpus);
@@ -57,11 +76,15 @@ public class InfoRet {
 			results = BM25(query, corpus);
 		}
 
-		results.sort(new JOComparator());
-
+		Collections.sort(results, new JOComparator());		
+		
 		printResults(results);
 	}
 
+	/**
+	 * 
+	 * @param results
+	 */
 	private static void printResults(ArrayList<JSONObject> results) {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(results.get(i).getName() + " - "
@@ -69,10 +92,19 @@ public class InfoRet {
 		}
 	}
 
+	/**
+	 * 
+	 * @param query
+	 * @param corpus
+	 * @return corpus with scores
+	 */
 	private static ArrayList<JSONObject> BM25(String query,
 			ArrayList<JSONObject> corpus) {
+		
+		//TODO - refactor
+		
 		double avgdl=0;
-		System.out.println(corpus.size() + "..." + query.split(" ").length);
+//		System.out.println(corpus.size() + "..." + query.split(" ").length);
 		for(int i=0;i<corpus.size();i++){
 			avgdl += corpus.get(i).getBody().split(" ").length;
 		}
@@ -90,7 +122,7 @@ public class InfoRet {
 					}
 				}
 				int frequency = corpi.getBody().toLowerCase().split(query.split(" ")[i].toLowerCase()).length-1;
-				System.out.println(frequency + " ");
+//				System.out.println(frequency + " ");
 				IDF = Math.log10((totDoc - numDocMatch + 0.5) / (numDocMatch + 0.5));
 				double k1 = 1.2;
 				double b = .75;
